913c69e6ca50ef543cb603df214975a9dc4c1eba
unknown <pavithrasivanoli@gmail.com>
2022-12-16 22:40:32 +0530

    updated the files

---
+++ js/new.js
@@ -0,0 +1,21 @@
+//for in loop
+ /*var bottle={
+	color:"yellow",
+	shape:"cylinder",
+	size:{
+		height:"100px",
+		width:"20px",
+	}
+}
+for (a in bottle)
+{
+	console.log(bottle[a])
+	console.log(a)
+	console.log(a+":"+bottle[a]);
+}*/
+//for of loop
+/*var b =[10,20,30,40]
+for(a of b)
+{
+	console.log(a)
+}*/
